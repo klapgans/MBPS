6a27d7f0df221b4f6aade413d4d478fa720f442e
klapgans <jeroen@inenergie.nl>
2024-11-12 14:38:37 +0100

    converted weatherdata

---
+++ data/practical_data/transform_weather.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import os
+
+
+def transform_weather_data(data_dir, csv_files):
+    data = {}
+    for csv_file in csv_files:
+        file_path = os.path.join(data_dir, csv_file)
+        df = pd.read_csv(file_path, comment='#', header=None, names=['STN', 'YYYYMMDD', 'TG', 'SQ', 'Q'])
+        data[csv_file] = df
+    return data
+
+def convert_units(data):
+    for data_file, df in data.items():
+        df['TG'] = df['TG'] * 10
+        df['Q'] = df['Q'] * 10000
+        df["date"] = pd.to_datetime(df["YYYYMMDD"], format='%Y%m%d')
+        df["day_nr"] = df["date"].dt.dayofyear
+        data[data_file] = df
+
+    return data
+
+
+def save_weather_data(data, output_dir):
+    for data_file, df in data.items():
+        output_file = os.path.join(output_dir, data_file)
+        df.to_csv(output_file, index=False)
+    return None
+
+
+data = transform_weather_data('data/practical_data', ['weather_2001.csv', 'weather_2003.csv', 'weather_2007.csv'])
+
+data = convert_units(data)
+
+save_weather_data(data, 'data/practical_data/converted')
+
--- practicals/grass_test.py
+++ practicals/grass_test.py
@@ -18,7 +18,7 @@ tsim = np.linspace(0.0, 365.0, 365+1) # [d]
 dt = 1 # [d]
 # Initial conditions
 # TODO: Define sensible values for the initial conditions
-x0 = {'Ws':0.1,'Wg':0.1} # [kgC m-2]
+x0 = {'Ws':1,'Wg':1} # [kgC m-2]
 # Model parameters (as provided by Mohtar et al. 1997 p.1492-1493)
 # TODO: Define values for the model parameters
 p = {'a':40.0,          # [m2 kgC-1] structural specific leaf area
